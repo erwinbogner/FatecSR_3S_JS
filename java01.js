63a5dabb4b8171ee087270d2de0498879ed62f99
ROBERT SILVA <robert.silva4@fatec.sp.gov.br>
2020-05-30 15:54:35 -0300

    update

--- java01.js
+++ java01.js
@@ -1,17 +1,65 @@
+//const URL_BASE = "https://wllsistemas.com.br/api/v2/public/pessoa/"
+//const URL_BASE = "https://www.wllsistemas.com.br/api/v3/cliente/"
 const URL_BASE = "https://wllsistemas.com.br/api/v2/public/pessoa/"
+CarregarDados();
+
+
+
+botaoCarregar.addEventListener('click',
+    CarregarDados
+);
+
+
+function CarregarDados() {
+    var str = '';
+    resultado.value = '';
+    dados1.innerHTML = '';
+    fetch(URL_BASE, { method: 'GET' })
+        .then(response => response.json())
+        .then(json => {
+            json.forEach(pessoas =>
+                dados1.innerHTML += `<tr class="table-striped"><td> ${pessoas.ID}</td><td>${pessoas.NOME}</td><td>${pessoas.EMAIL}</td><td>${pessoas.TIPO}</td><td><button class="btn btn-primary">Alterar</button> <button class="btn btn-danger">Excluir</button></td></tr>`
+            );
+        });
+    //dados1.innerHTML = str;
+    //console.log(str);
+    //dados1.innerHTML = `<tr><td>pessoas.ID</td><td>pessoas.NOME</td><td>pessoas.EMAIL</td><td>pessoas.TIPO</td></tr>`;
+
+}
+
+//resultado.value += pessoas.ID + '-' + pessoas.NOME + '-' + pessoas.EMAIL + '\n');
+//str += "<div id='botao'> <p>" + pessoas.NOME + "</p> " + "</div>");
+
 
 GravarDado.addEventListener('click', function() {
-    let nome = nome.value
-    let email = email.value
-    let tipo = tipo.value
+    let xnome = nome.value
+    let xemail = email.value
+    let xtipo = tipo.value
     fetch(URL_BASE, {
-        method : 'POST',
-        body: 'NOME='+ nome.value+'&EMAIL='+email.value+'&TIPO='+tipo.value+'',
-        headers:{
-            'Content-Type' : 'application/x-www-form-urlencoded'
-        }
-    })
-    .then(response => response.json())
-    .then(json =>console.log(json.mensagem))
+            method: 'POST',
+            body: `nome=${xnome}&email=${xemail}&tipo=${xtipo}`,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        })
+        .then(response => response.json())
+        //.then(json => console.log(json.mensagem))
+        .then(json => resposta.innerHTML = 'Incluido com sucesso . . . ')
 
-})
+});
+
+//`NOME=${xnome}&EMAIL=${xemail}&TIPO=${xtipo}`,
+//body: 'NOME=' + xnome + '&EMAIL=' + xemail + '&TIPO=' + xtipo,
+
+botaoADD.addEventListener('click', () => {
+    fetch(URL_BASE, {
+            method: 'POST',
+            body: `nome=ErwinBogner&email=bonito@bonito.com&tipo=FISICA`,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        })
+        .then(response => response.json())
+        //.then( json => console.log( json.mensagem))
+        .then(json => resposta.innerHTML = 'Incluido com sucesso')
+});
