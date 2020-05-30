da6415cfa7621f18451dd22bc6e5f50a2678df77
ROBERT SILVA <robert.silva4@fatec.sp.gov.br>
2020-05-29 21:09:13 -0300

    add2

--- java01.js
+++ java01.js
@@ -1,2 +1,17 @@
-GravarDado.addEventListener('click', ()=>{
+const URL_BASE = "https://wllsistemas.com.br/api/v2/public/pessoa/"
+
+GravarDado.addEventListener('click', function() {
+    let nome = nome.value
+    let email = email.value
+    let tipo = tipo.value
+    fetch(URL_BASE, {
+        method : 'POST',
+        body: 'NOME='+ nome+'&EMAIL='+email+'&TIPO='+tipo+'',
+        headers:{
+            'Content-Type' : 'application/x-www-form-urlencoded'
+        }
+    })
+    .then(response => response.json())
+    .then(json =>console.log(json.mensagem))
+
 })
